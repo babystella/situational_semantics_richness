13f6cc05df9afdc178dc0022af82bf7ce57f3052
babystella <42821988+babystella@users.noreply.github.com>
2023-05-19 14:01:05 +0100

    Update situational_semantics_richness.cpp

--- src/situational_semantics_richness.cpp
+++ src/situational_semantics_richness.cpp
@@ -9,19 +9,15 @@
 #include <message_filters/subscriber.h>
 #include <message_filters/synchronizer.h>
 #include <message_filters/sync_policies/approximate_time.h>
-#include <chrono>
-
-
 #include <numeric>
 #include <iostream>
 #include <fstream>
-#include <tuple>
+#include <cmath>
 #include "ros/time.h"	
 
 #include <image_transport/image_transport.h>
 #include <opencv2/highgui/highgui.hpp>
 #include <cv_bridge/cv_bridge.h>
-
 
 #include "sha/sha.h"
 #include "radiation/sievert.h"
@@ -38,40 +34,129 @@ using namespace sha;
 using namespace radiation;
 
 
-
-
 float sv;
 float shascore;
 float ssr;
-
+std::vector<double> weight = {1,1};
 
 int lowscore_cnt = 0;
 int highscore_cnt = 0;
 int zero_cnt = 0;
+const int ARRAY_SIZE = 5;
 
 
 
+// void expert_weight(const std::vector<double>& inputs)
+// {
+	
+
+// }
+
+
+std::vector<double> softmax(const std::vector<double>& inputs) 
+{
+    std::vector<double> softmaxresult;
+    double sumExp = 0.0;
+
+    // Compute exponentials and sum of exponentials
+    for (const auto& input : inputs) {
+        double expVal = std::exp(-(input - std::pow(input, 2)) / 2.0);
+        softmaxresult.push_back(expVal);
+        sumExp += expVal;
+    }
+    // Normalize by dividing by the sum of exponentials
+    for (auto& softmaxresults : softmaxresult) {
+        softmaxresults /= sumExp;
+    }
+
+    return softmaxresult;
+}
+
+double dotProduct(const std::vector<double>& v1, const std::vector<double>& v2) 
+{
+    // Check if the vectors have the same size
+    if (v1.size() != v2.size()) {
+        std::cerr << "Error: Vectors must have the same size." << std::endl;
+        return 0.0;  // Return an error value
+    }
+
+    double result = 0.0;
+
+    // Calculate the dot product
+    for (size_t i = 0; i < v1.size(); ++i) {
+        result += v1[i] * v2[i];
+    }
+
+    return result;
+}
+
+double attention(double newSSR)
+{
+
+    std::vector<double> SSRArray(ARRAY_SIZE, 0.0);  // Initialize array with zeros
+	std::vector<double> softmaxArray(ARRAY_SIZE, 0.0);
+	std::vector<double> time = {1,2,3,4,5};
+    int currentIndex = 0;  // Tracks the current index to update
+	double SSR_hat;
+    // Add numbers to the array until the user inputs a negative number
+
+	
+	if (SSRArray.size() < ARRAY_SIZE)
+	{
+		SSRArray.push_back(newSSR);
+
+	}
+	else
+	{
+		SSRArray.erase(SSRArray.begin());
+		SSRArray.push_back(newSSR);
+	}
+	
+	SSR_hat = dotProduct(softmax(time), SSRArray);
+
+	return SSR_hat; 
+
+}
 
 void processcallback(const radiation::sievertConstPtr msg1, const sha::shaConstPtr msg2)
 { 
     // ros::Time msg_time = msg1->header.stamp;
-	ROS_INFO("Processing");
+	
+	std::vector<std::array<double, ARRAY_SIZE>> matrix;
+
+    std::vector<double> ssrVector;
+	std::array<double, ARRAY_SIZE> riskArray;
+    std::array<double, ARRAY_SIZE> SHAArray;
+    std::array<double, ARRAY_SIZE> radiationArray;
+	std::array<double, ARRAY_SIZE> noiseArray;
+
+
 	int SV_timestamp = msg1->header.stamp.toSec();
 	int shascore_timestamp = msg2->header.stamp.toSec();
+
 
 	float SV = msg1->sv;
 	float shascore = msg2->SHAScore;
 	
-    // std::cout<< "processcallback is running" << std::endl;
-	float ssr =  SV*1000 + shascore;
-	ROS_INFO("sv:%.3f", ssr);
+	// build ssr vector
+	ssrVector.push_back(SV);
+	ssrVector.push_back(shascore);
 	
-	SSRscore.ssr = ssr;
+	// process ssr and ssr_hat
+	double ssr = dotProduct(weight, ssrVector);
+	double ssr_hat = attention(ssr);
+	
+
+	ROS_INFO("ssr:%.3f", ssr);
+	ROS_INFO("ssr_hat:%.3f", ssr_hat);
+
+	SSRscore.ssr = ssr_hat;
 		
 		// set the message timestamp
 	SSRscore.header.stamp = ros::Time::now();
 
 	ssr_pub.publish(SSRscore);
+
 	
 	if (ssr > 0.9)
 		{
@@ -126,12 +211,6 @@ void processcallback(const radiation::sievertConstPtr msg1, const sha::shaConstP
 
 
 }
-
-
-
-
-
-
 
 
 int main(int argc, char *argv[])
